8dd423992e4a5a21c6a683448b2ef8c1ad516bfd
Hemanth <Hemanthakumar.reddyvaari@honeywell.com>
2020-01-05 15:22:45 +0530

    Config files

---
+++ src/config/magento.js
@@ -0,0 +1,9 @@
+export const magentoOptions = {
+  url: 'https://www.durgaveg.com/',
+  home_cms_block_id: '58',
+  authentication: {
+    integration: {
+      access_token: 'zttcv4znha3xizgpjgchwccf6nmvpsec'
+    }
+  }
+};
